7821bdc81b8c2b3612d7e8512519003709dbfae1
veds12 <vedantshah2012@gmail.com>
2020-11-05 11:45:01 +0530

    added team stats

--- src/data_collection/ncaa_scraper.py
+++ src/data_collection/ncaa_scraper.py
@@ -11,6 +11,8 @@ from bs4 import BeautifulSoup
 
 user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
 
+year = "2019"
+
 url_team_map = {
     "Baylor": "https://stats.ncaa.org/teams/481920",  # Use links according to the year
     "Stanford": "https://stats.ncaa.org/teams/481752",
@@ -24,19 +26,30 @@ url_team_map = {
     "Florida": "https://stats.ncaa.org/team/235.0/14942",
 }
 
-print("<-------Starting Data Scraping------->")
+year = "2019"
+
+print("<-------Starting Team Data Scraping------->")
 for team_name, root_url in url_team_map.items():
     req = Request(root_url, headers={"User-Agent": user_agent})
     root_page = urlopen(req)
     root_html = root_page.read().decode("utf-8")
     soup = BeautifulSoup(root_html, "lxml")
+    team_stats_url = "https://stats.ncaa.org" + str(
+        soup.find("body").find("div", id="contentarea").contents[15]["href"]
+    )
     game_by_game_url = "https://stats.ncaa.org" + str(
         soup.find("body").find("div", id="contentarea").contents[17]["href"]
     )
     r = requests.get(game_by_game_url, headers={"User-Agent": user_agent})
     tables = pd.read_html(r.text)
     df = tables[-1].drop(labels=[0], axis=0)
-    path = "./../data/ncaa/game_by_game_" + team_name + ".csv"
+    path = f"./../../data/ncaa/{year}/team_game_by_game/{team_name}.csv"
+    df.to_csv(path, index=False)
+
+    r = requests.get(team_stats_url, headers={"User-Agent": user_agent})
+    tables = pd.read_html(r.text)
+    df = tables[1]
+    path = f"./../../data/ncaa/{year}/team_stats/{team_name}.csv"
     df.to_csv(path, index=False)
     print(f"{team_name} - Scraped!")
 print("<-------Data Scraped!------->")
